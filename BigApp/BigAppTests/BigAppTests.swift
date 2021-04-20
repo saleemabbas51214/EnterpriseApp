3f045011c48363ea0a7f063f990e969ab1cbff36
saleemabbas <saleem@straffr.com>
2021-04-20 04:44:53 +0200

    wrote a small test case

--- BigApp/BigAppTests/BigAppTests.swift
+++ BigApp/BigAppTests/BigAppTests.swift
@@ -21,13 +21,8 @@ class BigAppTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        let i = 2
+        XCTAssertEqual(i*2, 4)
     }
 
 }
